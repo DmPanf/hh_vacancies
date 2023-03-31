749356c504d57772eac19139f46c5b807dfbecbf
Dmitry <99917230+dnp34@users.noreply.github.com>
2023-03-31 18:51:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# 1. Установить Poetry на свой компьютер, если он еще не установлен
 curl -sSL https://install.python-poetry.org | python3 -
 
-poetry init
-
+# 2. установить все зависимости, указанные в pyproject.toml
 poetry install
 
-poetry shell
+# 
+# poetry shell
 
+# 3. запустить стартовый скрипт проекта
 poetry run python main.py
